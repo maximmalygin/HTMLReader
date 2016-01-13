e0a2f72a60fe79c7708548b93adacd6d9ce296b8
Maksym Malyhin <malyginmaxim@gmail.com>
2016-01-13 18:31:17 +0100

    Bridging header added to make framework visible in Swift as a module.

---
+++ Framework/HTMLReader-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  HTMLReader-Bridging-Header.h
+//  HTMLReader
+//
+//  Created by Maxim Malyhin on 1/13/16.
+//  Copyright © 2016 Nolan Waite. All rights reserved.
+//
+
+#ifndef HTMLReader_Bridging_Header_h
+#define HTMLReader_Bridging_Header_h
+
+#import "HTMLReader.h"
+#import "HTMLSupport.h"
+
+#endif /* HTMLReader_Bridging_Header_h */
